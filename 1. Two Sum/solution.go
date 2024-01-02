51b42f3bc1bd4f1c30ed30f01d5130a609e71635
gajevski <mikolajxgajewski@gmail.com>
2024-01-02 13:27:27 +0100

    Two sum Go simple fix

--- 1. Two Sum/solution.go
+++ 1. Two Sum/solution.go
@@ -2,8 +2,7 @@ func twoSum(nums []int, target int) []int {
     for i := 0; i < len(nums); i++ {
         for j := i + 1; j < len(nums); j++ {
             if nums[i] + nums[j] == target {
-                solution := []int{i, j}
-                return solution;
+                return []int{i, j};
             }
         }    
     }
